6b73690e046600e8d2bb115485b8e4d0b7afea1f
Cindy Liang <hakkacindy@gmail.com>
2021-07-09 23:16:13 +0100

    add class to show the html in frontend

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = (props) => {
+//   return (
+//     <div className="container">
+//       <h1> Hello, {props.name}</h1>
+//     </div>
+//   );
+// }
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  render() {
+    return (
+      <h1><strong>Hello, {this.props.name}</strong></h1>
+    )
+  }
+}
 
 const root = document.getElementById('root');
+
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <Hello name="Cindy" />,
+    root);
 }
