f751a569d058d494ae15687592fa6b033769f944
Cindy Liang <hakkacindy@gmail.com>
2021-07-14 00:06:16 +0100

    giphy api call

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
@@ -10,7 +12,28 @@ class App extends Component {
     this.state = {
       gifts: [],
       selectedGifId:"5bdhq6YF0szPaCEk9Y"
-    }
+    };
+    this.search("cutie");
+    this.selectGif = this.selectGif.bind(this);
+  }
+
+  search = (query) => {
+    // API call
+    giphy('nE5e0lRHeSZr0DEAAT8QOVkX4cZWGQQx').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifts: result.data
+      });
+    });
+  }
+
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
   }
 
   render() {
@@ -22,13 +45,13 @@ class App extends Component {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
             <Gif id= {this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifts} />
+          <GifList gifs={this.state.gifts} selectGif={this.selectedGif} />
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleImageUpdate = (event) => {
+    this.props.selectGif
+  }
   render() {
     const src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`
     return (
-      <img src={src} height="400" alt="" className="gif" />
+      <img src={src} height="400" alt="" className="gif" 
+        onClick={this.handleImageUpdate}/>
     )
   }
 }
